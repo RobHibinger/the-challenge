dcd5aab505386f148d7a971898d05b02c45c6e37
Rob Hibinger <robhibinger@gmail.com>
2025-02-02 18:24:54 -0500

    Add game object, velocity, collision, and reset game state if outside

--- c1-pong/main.cpp
+++ c1-pong/main.cpp
@@ -7,12 +7,56 @@
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
 
-const float GAME_SPEED = 500.0f;
+const float GAME_SPEED = 250.0f;
+
+struct Vec2f {
+    float x = 0.0f, y = 0.0f;
+};
+
+struct GameObject {
+    SDL_FRect rect = {0.0f, 0.0f, 0.0f, 0.0f};
+    Vec2f vel;
+};
 
 struct GameState {
-    SDL_FRect left_paddle;
-    SDL_FRect right_paddle;
-    SDL_FRect ball;
+    GameObject left_paddle;
+    GameObject right_paddle;
+    GameObject ball;
+
+    static GameState DefaultGameState() {
+        float paddle_width = 20.0f;
+        float paddle_height = 100.0f;
+        float ball_width = 20.0f;
+        float balll_height = 20.0f;
+        
+        return GameState {
+            .left_paddle = GameObject {
+                .rect = SDL_FRect {
+                    .x = 0,
+                    .y = WINDOW_HEIGHT / 2 - paddle_height / 2,
+                    .w = paddle_width,
+                    .h = paddle_height,
+                },
+            },
+            .right_paddle = GameObject {
+                .rect =  SDL_FRect {
+                    .x = WINDOW_WIDTH - paddle_width,
+                    .y = WINDOW_HEIGHT / 2 - paddle_height / 2,
+                    .w = paddle_width,
+                    .h = paddle_height,
+                },
+            },
+            .ball = GameObject {
+                .rect  = SDL_FRect {
+                    .x = WINDOW_WIDTH / 2 - ball_width / 2,
+                    .y = WINDOW_HEIGHT / 2 - balll_height / 2,
+                    .w = ball_width,
+                    .h = balll_height,
+                },
+                .vel = {1.0f, 1.0f},
+            },
+        };
+    }
 };
 
 struct AppState {
@@ -26,24 +70,51 @@ struct AppState {
 };
 
 void process_input(AppState* as) {
+    as->game_state.left_paddle.vel.y = 0;
     const bool* keyboardState = SDL_GetKeyboardState(nullptr);
     if (keyboardState[SDL_SCANCODE_W]) {
-        as->game_state.left_paddle.y -= 1.0f;
+        as->game_state.left_paddle.vel.y = -1.0f;
     }
     if (keyboardState[SDL_SCANCODE_S]) {
-        as->game_state.left_paddle.y += 1.0f;
+        as->game_state.left_paddle.vel.y = 1.0f;
     }
 }
 
-void update_game(AppState* as) {
-    as->game_state.ball.x += GAME_SPEED * as->delta_time;
+bool is_colliding_aabb(const GameObject& o1, const GameObject& o2) {
+    return o1.rect.x < o2.rect.x + o2.rect.w &&
+        o1.rect.x + o1.rect.w > o2.rect.x &&
+        o1.rect.y < o2.rect.y + o2.rect.h &&
+        o1.rect.y + o1.rect.h > o2.rect.y;
+}
 
-    
+void update_game(AppState* as) {
+    float right_paddle_ball_diff = as->game_state.right_paddle.rect.y + as->game_state.right_paddle.rect.h / 2 - as->game_state.ball.rect.y + as->game_state.ball.rect.h / 2;
+    as->game_state.right_paddle.vel.y = 0;
+    if (right_paddle_ball_diff != 0) {
+        as->game_state.right_paddle.vel.y = right_paddle_ball_diff > 0 ? -1.0f : 1.0f;
+    }
+
+    if (as->game_state.ball.rect.y <= 0 || as->game_state.ball.rect.y + as->game_state.ball.rect.h >= WINDOW_HEIGHT) {
+        as->game_state.ball.vel.y = -1.0f * as->game_state.ball.vel.y;
+    }
+
+    if (is_colliding_aabb(as->game_state.right_paddle, as->game_state.ball) || is_colliding_aabb(as->game_state.left_paddle, as->game_state.ball)) {
+        as->game_state.ball.vel.x = -1.0f * as->game_state.ball.vel.x;
+    }
+
+    if (as->game_state.ball.rect.x < 0 || as->game_state.ball.rect.x > WINDOW_WIDTH) {
+        as->game_state = GameState::DefaultGameState();
+    }
+
+    as->game_state.left_paddle.rect.y += GAME_SPEED * as->delta_time * as->game_state.left_paddle.vel.y * 2.0f;
+    as->game_state.right_paddle.rect.y += GAME_SPEED * as->delta_time * as->game_state.right_paddle.vel.y * 1.0f;
+    as->game_state.ball.rect.x += GAME_SPEED * as->delta_time * as->game_state.ball.vel.x;
+    as->game_state.ball.rect.y += GAME_SPEED * as->delta_time * as->game_state.ball.vel.y;
 
     SDL_SetRenderDrawColor(as->renderer, 255, 255, 255, 255);
-    SDL_RenderFillRect(as->renderer, &as->game_state.left_paddle);
-    SDL_RenderFillRect(as->renderer, &as->game_state.right_paddle);
-    SDL_RenderFillRect(as->renderer, &as->game_state.ball);
+    SDL_RenderFillRect(as->renderer, &as->game_state.left_paddle.rect);
+    SDL_RenderFillRect(as->renderer, &as->game_state.right_paddle.rect);
+    SDL_RenderFillRect(as->renderer, &as->game_state.ball.rect);
 }
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
@@ -58,34 +129,10 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
         return SDL_APP_FAILURE;
     }
 
-    float paddle_width = 20.0f;
-    float paddle_height = 100.0f;
-    float ball_width = 20.0f;
-    float balll_height = 20.0f;
-
    *appstate = new AppState {
         .window = window,
         .renderer = renderer,
-        .game_state = GameState {
-            .left_paddle = SDL_FRect {
-                .x = 0,
-                .y = WINDOW_HEIGHT / 2 - paddle_height / 2,
-                .w = paddle_width,
-                .h = paddle_height,
-            },
-            .right_paddle = SDL_FRect {
-                .x = WINDOW_WIDTH - paddle_width,
-                .y = WINDOW_HEIGHT / 2 - paddle_height / 2,
-                .w = paddle_width,
-                .h = paddle_height,
-            },
-            .ball = SDL_FRect {
-                .x = WINDOW_WIDTH / 2 - ball_width / 2,
-                .y = WINDOW_HEIGHT / 2 - balll_height / 2,
-                .w = ball_width,
-                .h = balll_height,
-            }
-        }
+        .game_state = GameState::DefaultGameState(),
     };
 
     return SDL_APP_CONTINUE;
